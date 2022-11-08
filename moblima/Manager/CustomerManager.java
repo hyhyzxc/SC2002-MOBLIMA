d7710bc8926c2b60eba2af4d9e9914c3f3c1271c
BananaThief99 <bananathief1999@gmail.com>
2022-11-08 17:48:47 +0800

    Javadoc

--- moblima/Manager/CustomerManager.java
+++ moblima/Manager/CustomerManager.java
@@ -10,10 +10,26 @@ import moblima.UI.LoginUI;
 
 import java.io.*;
 
+/**
+ * Represents a CustomerManagers.
+ * Concrete class that implements the AccountManager interface.
+ * @author
+ * @version 1.0
+ * @since 2022-11-08
+ */
 public class CustomerManager implements AccountManager {
+    /*
+     * ArrayList consisting of valid CustomerAccounts.
+     */
     private ArrayList<CustomerAccount> accountList;
+    /**
+     * Number of valid accounts.
+     */
     private int numAccounts;
 
+    /**
+     * Creates a new CustomerManager. 
+     */
     public CustomerManager() {
         CustomerAccountSerializer s = new CustomerAccountSerializer();
         accountList = s.getCustomerAccounts("CustomerAccountData.ser");
@@ -23,6 +39,12 @@ public class CustomerManager implements AccountManager {
         numAccounts = accountList.size();
     }
 
+    /**
+     * Creates a new account for a customer with the given owner, username, password.
+     * @param owner This CustomerAccount's owner.
+     * @param username This CustomerAccount's username.
+     * @param password This CustomerAccount's password.
+     */
     public void createNewAccount(User owner, String username, String password) {
         CustomerAccountSerializer s = new CustomerAccountSerializer();
         CustomerAccount newAccount = new CustomerAccount((Customer) owner, username, password);
@@ -32,6 +54,12 @@ public class CustomerManager implements AccountManager {
         System.out.println("Customer Account Successfully Created.");
     }
 
+    /**
+     * Verifies the validity of the given username and password.
+     * @param username This CustomerAccount's username.
+     * @param password This CustomerAccount's password.
+     * @return an Account if given details are valid, else null.
+     */
     public CustomerAccount validateLoginDetails(String username, String password) {
         CustomerAccountSerializer s = new CustomerAccountSerializer();
         accountList = s.getCustomerAccounts("CustomerAccountData.ser");
